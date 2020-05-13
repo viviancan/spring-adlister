a8626e5a4f86e915fc5d5c56c0654125c54fa08a
viviancan <vcnales@gmail.com>
2020-05-12 21:57:21 -0500

    Basic updates to prepare for Repositories and JPA lesson

--- src/main/java/com/edevs/springadlister/controllers/AdController.java
+++ src/main/java/com/edevs/springadlister/controllers/AdController.java
@@ -1,32 +1,53 @@
 package com.edevs.springadlister.controllers;
 
+import com.edevs.springadlister.models.Ad;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
+@Controller
 public class AdController {
 
+    @GetMapping("/")
+    public String welcome(){
+        return "index";
+    }
+
     @GetMapping("/ads")
-    @ResponseBody
-    public String showAds(){
-        return "This page will show the ads";
+    public String indexAds(Model model){
+        ArrayList<Ad> adList = new ArrayList<>();
+
+        adList.add(new Ad(1, "TV", "Old TV. $200 OBO"));
+        adList.add(new Ad(1, "Bicycle", "Getting rid of my bike! Make an offer."));
+        adList.add(new Ad(1, "Super cool gaming system", "Gaming system for sale! $100"));
+
+        model.addAttribute("ads", adList);
+
+        return "ads/index";
     }
 
     @GetMapping("/ads/{id}")
-    @ResponseBody
-    public String showAd(@PathVariable  int id){
-        System.out.println("This is the ID of the ad");
-        return "This page will show a single ad";
+    public String showAd(@PathVariable long id, Model model){
+        System.out.println(id);
+
+        Ad newAd = new Ad(id, 1, "Puppy Crate", "New puppy crate!! My dog already outgrew it... ");
+        model.addAttribute("ad", newAd);
+
+        return "ads/show";
     }
 
-    @GetMapping("/posts/create")
+    @GetMapping("/ads/create")
     @ResponseBody
     public String showCreateAd(){
         return "This page will show a form to create an ad";
     }
 
-    @PostMapping("/posts/create")
+    @PostMapping("/ads/create")
     @ResponseBody
     public String createAd(){
         return "This endpoint will create the ad";
